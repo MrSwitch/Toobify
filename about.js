59ac741a239a7e1a34f5d8f0dcde08354f25497e
Andrew Dodson <sonofdod@gmail.com>
2012-05-21 22:25:47 +1000

    Adding About link

---
+++ about.js
@@ -0,0 +1,8 @@
+// About.js
+// Displays information about the website
+
+$(function(){
+	$('button.about').click(function(){
+		$('<h2>About</h2><p>Toobify is designed and developed for me - <a href="http://twitter.com/#!mr_switch" target="_blank">@mr_switch</a></p>').alert();	
+	});
+})
